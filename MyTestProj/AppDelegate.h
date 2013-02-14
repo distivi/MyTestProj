7fd4846d616843106f2efea598d5416bdb1cf825
Max <max.norb.91@gmail.com>
2013-02-14 22:41:51 +0200

    Create project

---
+++ MyTestProj/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MyTestProj
+//
+//  Created by Maxim Kulik on 2/14/13.
+//  Copyright (c) 2013 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MyTestProj/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyTestProj
+//
+//  Created by Maxim Kulik on 2/14/13.
+//  Copyright (c) 2013 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
